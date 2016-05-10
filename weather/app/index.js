35333f516cba574dc7c19bb11b83c5aa8158dd63
Anthony Ng <newyork.anthonyng@gmail.com>
2016-05-09 22:36:14 -0400

    Add router

--- weather/app/index.js
+++ weather/app/index.js
@@ -1,12 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var Hello = React.createClass({
-	render: function() {
-		return (
-			<div>Hello World!</div>
-		);
-	}
-});
-
-ReactDOM.render(<Hello />, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
