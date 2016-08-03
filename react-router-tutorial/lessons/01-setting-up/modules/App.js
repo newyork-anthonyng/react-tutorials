28fb3e594496a424134444a546963a784e7677d3
Anthony Ng <newyork.anthonyng@gmail.com>
2016-08-02 20:15:19 -0400

    Finish react-router tutorial

--- react-router-tutorial/lessons/01-setting-up/modules/App.js
+++ react-router-tutorial/lessons/01-setting-up/modules/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { IndexLink } from 'react-router';
 import NavLink from './NavLink';
+import Home from './Home';
 
 export default React.createClass({
   render() {
@@ -7,8 +9,9 @@ export default React.createClass({
 			<div>
 				<h1>React Router Tutorial</h1>
 				<ul role="nav">
-					<li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-					<li><NavLink to="/repos" activeClassName="active">Repos</NavLink></li>
+					<li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
+					<li><NavLink to="/about">About</NavLink></li>
+					<li><NavLink to="/repos">Repos</NavLink></li>
 				</ul>
 
 				{this.props.children}
